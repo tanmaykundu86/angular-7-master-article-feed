f6b8b55b0dffcbcbc2748521e8de7c53e0982df5
tanmaykundu86 <tanmaykundu86@gmail.com>
2019-10-30 15:48:23 +0530

    1 file modified localstorage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ export class AppComponent implements OnInit {
 
   constructor(private dataService: DataService) {}
   ngOnInit() {
-   // localStorage.setItem('articleData', '');
+   localStorage.setItem('articleData', '');
    this.dataService.setDataToLocal();
   }
 }
